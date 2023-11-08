b0d037342c3ac692bbca3d33a8759416838aed62
Jayasankari_Raajkomahan <shanksankari@gmail.com>
2023-11-08 22:28:29 +0530

    first commit

---
+++ Travel_Front-main/src/utils/config.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://travel-be-0cr6.onrender.com/api/v1'
+// export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
---
+++ Travel_SE_BE-main/routes/reviews.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { createReview } from '../Controllers/reviewController.js'
+
+
+const router = express.Router()
+
+router.post('/:tourId', createReview)
+
+
+
+export default router
